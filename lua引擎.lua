15eb1d207535dec61117aed888ed17aa2586ae71
最萌小汐 <sumneko@hotmail.com>
2019-01-17 18:25:49 +0800

    改一下写法

--- lua引擎.lua
+++ lua引擎.lua
@@ -19,18 +19,22 @@ local function injectJass(w2l, buf)
     if not buf then
         return nil
     end
-    local _, pos = buf:find('function main takes nothing returns nothing', 1, true)
+    local start, finish = buf:find('function main takes nothing returns nothing', 1, true)
     local bufs = {}
-    bufs[#bufs+1] = buf:sub(1, pos)
-    bufs[#bufs+1] = '//Lua 引擎开始\r\n'
-    bufs[#bufs+1] = [===[
-        call Cheat("exec-lua:_luapath")
-        call Cheat("exec-lua:_currentpath")
-        call Cheat("exec-lua:ac")
-        call Cheat("exec-lua:main")
-    ]===]
-    bufs[#bufs+1] = '//Lua 引擎结束'
-    bufs[#bufs+1] = buf:sub(pos+1)
+    bufs[#bufs+1] = buf:sub(1, start-1)
+    bufs[#bufs+1] = [[
+        //Lua 引擎开始\r\n
+        function War3LuaMapPlugin takes nothing returns nothing
+            call Cheat("exec-lua:_luapath")
+            call Cheat("exec-lua:_currentpath")
+            call Cheat("exec-lua:ac")
+            call Cheat("exec-lua:main")
+        endfunction
+        //Lua 引擎结束
+]]
+    bufs[#bufs+1] = buf:sub(start, finish)
+    bufs[#bufs+1] = '\r\ncall ExecuteFunc("War3LuaMapPlugin")'
+    bufs[#bufs+1] = buf:sub(finish+1)
     return table.concat(bufs)
 end
 
