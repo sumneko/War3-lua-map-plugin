25f5ed66c20362ec07b5cfd96c4f8a9c62708cd4
最萌小汐 <sumneko@hotmail.com>
2019-01-17 14:49:49 +0800

    基础对话框

--- import/scripts/ac/dialog.lua
+++ import/scripts/ac/dialog.lua
@@ -1,18 +1,27 @@
 local jass = require 'jass.common'
+local hotkey = require 'ac.war3.hotkey'
 
-local Dialog = {}
+local DialogHandle = {}
 
-local mt = {}
-
-local function getDialog(player)
-    if Dialog[player] then
-        return Dialog[player]
-    end
-    local dialog = setmetatable({
-        _handle = jass.DialogCreate(),
-    }, mt)
+local function onClick(dialog, btn)
 end
 
+local function getDialogHandle(player)
+    if DialogHandle[player] then
+        return DialogHandle[player]
+    end
+    local handle = jass.DialogCreate()
+    local trg = jass.CreateTrigger()
+    jass.TriggerRegisterDialogEvent(trg, handle)
+    jass.TriggerAddCondition(trg, jass.Condition(function ()
+        local btn = jass.GetClickedButton()
+    end))
+
+    DialogHandle[player] = handle
+    return handle
+end
+
+local mt = {}
 mt.__index = mt
 mt.type = 'dialog'
 
@@ -20,9 +29,68 @@ function mt:__tostring()
     return ('{dialog|%s}'):format(self._handle)
 end
 
+function mt:setTitle(title)
+    if type(title) ~= 'string' then
+        return
+    end
+    self._title = title
+end
+
+function mt:addButton(name, key, description)
+    if type(name) ~= 'string' then
+        return
+    end
+    if description == nil then
+        description = name
+    else
+        description = tostring(description)
+    end
+    self._button[#self._button+1] = {
+        name = name,
+        hotkey = key,
+        description = description,
+    }
+end
+
+function mt:refresh()
+    jass.DialogClear(self._handle)
+    jass.DialogSetMessage(self._handle, self._title)
+    for _, button in ipairs(self._button) do
+        jass.DialogAddButton(self._handle, button.description, hotkey[button.hotkey] or 0)
+    end
+end
+
+function mt:show()
+    jass.DialogDisplay(self._owner._handle, self._handle, true)
+end
+
+function mt:hide()
+    jass.DialogDisplay(self._owner._handle, self._handle, false)
+end
+
 return function (player, data)
     if not ac.isPlayer(player) then
         return nil
     end
-    local dialog = getDialog(player)
+    if type(data) ~= 'table' then
+        return nil
+    end
+    local handle = getDialogHandle(player)
+
+    local dialog = setmetatable({
+        _owner = player,
+        _handle = handle,
+        _button = {},
+    }, mt)
+
+    dialog:setTitle(dialog, data[1])
+    for _, info in ipairs(data) do
+        if type(info) == 'table' then
+            dialog:addButton(info[1], info[2], info[3])
+        end
+    end
+    dialog:refresh()
+    dialog:show()
+
+    return dialog
 end
